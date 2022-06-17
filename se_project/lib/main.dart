939dec647b02933128e98be204465669f4ab4954
aneese421@gmail.com <aneese421@gmail.com>
2022-06-17 14:49:37 +0500

    init

--- se_project/lib/main.dart
+++ se_project/lib/main.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:se_project/categories.dart';
-import 'package:se_project/confirm.dart';
-import 'package:se_project/editAddress.dart';
-import 'package:se_project/login.dart';
-import 'package:se_project/checkout.dart';
-import 'package:se_project/profile.dart';
 import 'package:se_project/signup.dart';
-import 'addAddress.dart';
-import 'address.dart';
-import 'payment.dart';
 
 void main() => runApp(
     const MaterialApp(home: signup(), debugShowCheckedModeBanner: false));
-
-
-
-
-
